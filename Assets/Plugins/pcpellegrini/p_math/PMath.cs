59b4bb3561ff437903d22e29b4bf105ad2d90ec9
pcpellegrini <pcpellegrini@gmail.com>
2016-08-10 21:16:09 -0300

    First up

---
+++ Assets/Plugins/pcpellegrini/p_math/PMath.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static public class PMath {
+
+    /// <summary>
+    /// Returns a index of an array after calculate the probability of every array position
+    /// </summary>
+    /// <param name="p_percentages"> Inform an int array with the percentage of every item in the correct order ex:(20, 30, 50)</param>
+    static public int CalculateProb(int[] p_percentages)
+    {
+        int __sum = 0;
+        for (int i = 0; i < p_percentages.Length; i++)
+        {
+            int __num = i;
+            __sum += p_percentages[__num];
+        }
+        int __rndChoose = Random.Range(0, __sum);
+        int __tmpChance = 0;
+        int __idx = 0;
+        for (int i = 0; i < p_percentages.Length; i++)
+        {
+            int __num = i;
+            __tmpChance += p_percentages[__num];
+            if (__rndChoose < __tmpChance)
+            {
+                __idx = __num;
+                break;
+            }
+        }
+        return __idx;
+    }
+}
---
+++ Assets/Plugins/pcpellegrini/p_standarts.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class p_standarts : MonoBehaviour {
+    
+    // enum vars all in upper case
+    public enum VARENUM
+    {
+        TYPE1,
+        TYPE2,
+        TYPE3
+    }
+    // Public Vars (Public var always start with lower case and uppercase in word separation)
+    public VARENUM varType;
+    public event Action<string> varEvent;
+    public bool varBool;
+    public int varint;
+    public float varfloat;
+    public Collider varCollider;
+
+    // Private Vars (Private var start with a underline before the name)
+    private bool _varBoolPriv;
+    private int _varIntPriv;
+    private float _varFloatPriv;
+    private Collider _varColliderPriv;
+
+    // Unity methods
+    void Start()
+    {
+        // Local vars always start with a double underline
+        float __varfloatLocal = 0f;
+        __varfloatLocal++;
+    }
+    void OnCollisionEnter(Collision p_collision)
+    {
+        
+    }
+
+    // Public Methods
+    public void MethodName(bool p_value)
+    {
+        //Methods always start with uppercase in all words;
+        //Parameters always has a "p_" as prefix;
+    }
+
+    // Private Methods
+    private void _PrivMethodName(int p_value)
+    {
+        // Private methods start with a underline before name
+    }
+}
